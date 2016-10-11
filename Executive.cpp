2771ca154f644b5476394165cc842d131af803a9
Andy Monroe <jayhawker805@gmail.com>
2016-10-11 15:10:02 -0500

    Create basic run() and menu() methods in Executive

--- Executive.cpp
+++ Executive.cpp
@@ -1,3 +1,47 @@
 
 
 #include "Executive.h"
+
+void Executive::run()
+{
+    int choice = menu();
+
+    if (choice == 1)
+    {
+
+    }
+    else if (choice == 2)
+    {
+
+    }
+    else if (choice == 3)
+    {
+
+    }
+    else if (choice == 4)
+    {
+
+    }
+    else
+    {
+        //this won't be possible in the final program
+        std::cout << "ERROR. CRASHING NOW." << std::endl;
+    }
+}
+
+int Executive::menu()
+{
+    int choice = 0;
+
+    std::cout << "Select an option:" << std::endl;
+    std::cout << "1. List Reversal" << std::endl;
+    std::cout << "2. Compute c(n)" << std::endl;
+    std::cout << "3. Quadtree" << std::endl;
+    std::cout << "4. Quit" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Choice: ";
+    std::cin >> choice;
+
+    return(choice);
+}
--- Executive.h
+++ Executive.h
@@ -3,11 +3,15 @@
 #ifndef EXECUTIVE_H
 #define EXECUTIVE_H
 
+#include <iostream>
+
 class Executive
 {
     public:
+        void run();
 
     private:
+        int menu();
 
 };
 
